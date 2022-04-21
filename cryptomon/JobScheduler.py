bcad8ade66ec7a5c109aa331dbe948e103e01720
“Zephyrsz” <151493430@qq.com>
2022-04-21 18:35:58 +0800

    add meta, rank

--- cryptomon/JobScheduler.py
+++ cryptomon/JobScheduler.py
@@ -4,6 +4,8 @@
 # https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id=1&convertId=2781&timeStart=1637280000&timeEnd=1639785600
 # https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id={id}&convertId=2781&timeStart={starttime}&timeEnd={endtime}
 # 4)  dataparlser
+# 5 main page request
+#  https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=101&limit=100&sortBy=market_cap&sortType=desc&convert=USD,BTC,ETH&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap
 
 from datetime import datetime,timedelta,date
 
--- cryptomon/crypto_extractor.py
+++ cryptomon/crypto_extractor.py
@@ -76,27 +76,74 @@ class HtmlExtractor(object):
         id = dataIner['id']
         name = dataIner['name']
         quotes = dataIner['quotes']
-        for quote in data:
-            quote_data=quote['quote']
+        return quotes
+        # for quote in data:
+        #     quote_data=quote['quote']
 
     def timeStamp_generator(self, date_start, day_gap =60):
         pass
 
+class MainExtractor(object):
+    """
+    Extract the cryptocurrency price information from the website coinmarketcap.
+    By default, only the first 10 records (more than 10% of market cap) is recorded.
+    """
+    URL = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=3&sortBy=market_cap&sortType=desc&convert=USD"
+    # URL = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=3&limit=1&sortBy=market_cap&sortType=descconvert=USD&cryptoType=all"
+    # URL = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=3&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap"
 
+    def __init__(self):
+        """
+        Constructor
+        """
+        pass
+
+    def get_data(self):
+        """
+        Get data from the symbol and parse it.
+        :param symbol: Symbol
+        :returns Array of market volume
+        """
+        response = requests.get(MainExtractor.URL)
+        if response.status_code == 200:
+            return response
+
+    def data_parser(self,response):
+        data = json.loads(response.text)
+        crypoList= data['data']['cryptoCurrencyList']
+        return crypoList
+        # for quote in data:
+        #     quote_data=quote['quote']
+
+    def schema_map(self,crypoList):
+        pass
+
+
+import pprint
+extractor =MainExtractor()
+response = extractor.get_data()
+clist = extractor.data_parser(response)
+
+for c in clist:
+    pprint.pprint(c)
+    print("#################")
+
+# data = json.loads(response.text)
+# pprint.pprint(data)
 
 ###grace print json data
 import pprint
         
-if __name__ == '__main__':
-    extractor = HtmlExtractor()
-    id = 1
-    start = 1645228800
-    end = 1650326400
-    response = extractor.get_data(id,start,end)
-    data = extractor.history_parser(response)
-    # print(type(data))
-    for quote in data:
-        quote_data=quote['quote']
-        print(quote_data)
-    # pprint.pprint(data)
+# if __name__ == '__main__':
+#     extractor = HtmlExtractor()
+#     id = 1
+#     start = 1645228800
+#     end = 1650326400
+#     response = extractor.get_data(id,start,end)
+#     data = extractor.history_parser(response)
+#     # print(type(data))
+#     for quote in data:
+#         quote_data=quote['quote']
+#         print(quote_data)
+#     # pprint.pprint(data)
 
